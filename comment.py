e58f4d4955d3c5889bb5071c194b3ced38dbc74d
Sakda Rt <112610548+SakdaRt@users.noreply.github.com>
2023-06-21 10:54:31 +0700

    Add files via upload

---
+++ comment.py
@@ -0,0 +1,20 @@
+#comment.py
+#Creating a Comment
+print("Hello, World!")
+
+print("Coding!") #This is a comment
+
+#print("Cheers, Mate!")
+print("Cheers, Mate!")
+
+#This is a comment
+#written in
+#more than just one line
+print("Multiline Comments!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Multiline Comments!") 
