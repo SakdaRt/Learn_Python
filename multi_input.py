795dd917b1e4bb30609ea2ed54e076c2ae6ac744
Sakda Rt <112610548+SakdaRt@users.noreply.github.com>
2023-06-22 16:03:59 +0700

    Delete multi_input.py

--- multi_input.py
+++
@@ -1,6 +0,0 @@
-# multi_input.py
-name = input("What is your name?\n- ")
-ages = input("how old are you?\n- ")
-print("--------Output--------")
-print("My name is",name+".")
-print("I'm",ages,"years old.")
